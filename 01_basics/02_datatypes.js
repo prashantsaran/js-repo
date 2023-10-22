1a4a86ef5465f73c87bab74efd097cef0c9f730d
prashantsaran <72952601+prashantsaran@users.noreply.github.com>
2023-10-22 06:58:27 +0000

    data types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,22 @@
 //hello this message is from working branch
+"use strict"; // treats all JS code as newer version
+/*alert(2+3)
+we are using node js not browser
+*/
+
+/*
+Number 2 to the power 53
+BigInt
+String
+Boolean
+Symbol => used for uniqueness
+*/
+
+
+//  objects 
+let name="prashant"
+console.log(typeof name);
+/*
+null - standalone value(representation of empty value)
+undefined - when the value is not defined
+ */

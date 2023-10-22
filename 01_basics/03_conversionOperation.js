4e41f4d1c5d9a554f88b220b3f95376720581f84
prashantsaran <72952601+prashantsaran@users.noreply.github.com>
2023-10-22 07:40:11 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score=null
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//conversion to number
+// "33" => 33
+// "33abc" => Nan(Not a Number) (conversion to number)
+//true => 1; false=> 0 (conversion to number)
+
+
+/*
+conversion to boolean
+1=> true; 0=> false;
+""=> false
+"hitesh" => true
+ */
+
+
+/*
+conversion to string
+
+ */
+let someNumber=null;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)

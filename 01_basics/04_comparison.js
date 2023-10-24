a79c0655a594181d8c904634aa0c80971bf89831
prashantsaran <72952601+prashantsaran@users.noreply.github.com>
2023-10-24 07:21:12 +0000

    JS comparison of two different operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,6 @@
+/* Javascript allows compare two different datatype but Typescript does not
+It will not give you expected results */
+
+console.log(null>0) //false
+console.log(null==0); //false
+console.log(null>=0); //true

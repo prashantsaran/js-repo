ace2567cc9793103417d1dbe200682c355912a68
prashantsaran <72952601+prashantsaran@users.noreply.github.com>
2023-10-28 17:19:56 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,21 @@
+const name="prashant"
+const repoCount=50
+
+//using string interpolation with help of backtick ``
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+//another way of declaring string 
+
+const gameName=new String('Prashant')
+console.log(gameName);
+console.log(typeof gameName) //objects
+/*
+String {'Prashant'}
+0: "P"  -> on browser it is stored as key value pairs
+1: "r"
+2: "a"
+3: "s"
+4: "h"
+5: "a"
+6: "n"
+7: "t"
+*/

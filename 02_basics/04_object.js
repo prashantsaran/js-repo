0bdda14ed66dce56c41548b71ed617193420f642
prashantsaran <72952601+prashantsaran@users.noreply.github.com>
2024-01-17 12:13:03 +0000

    using object

---
+++ 02_basics/04_object.js
@@ -0,0 +1,62 @@
+const jsUser=new Object();
+
+const pythonUser = {}
+
+
+
+pythonUser.id=1
+pythonUser.name="Prashant"
+pythonUser.isLoggedIn=false
+pythonUser.newObject ={
+    email : "prashant.s@novelvox.com",
+    neighour:{
+        name:"devang mani tripathi",
+        rollNumber:34,
+        degree:"b-tech"
+    }
+}
+console.log('python user :',pythonUser)
+
+console.log('js user :',jsUser)
+
+console.log('accessing object using []',pythonUser["newObject"])
+
+//now this can also be written as :  
+
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a", 4:"b"}
+const obj3={...obj1,...obj2}
+console.log("final output",obj3);
+
+
+
+const course={
+    coursename: "javascript",
+    price: "999",
+    courseInstructor:"prashant"
+}
+
+
+//  destructoring 
+const {courseInstructor}=course // now we can use courseInstructor directly
+//this can also be written as , the difference is here we are changing name
+const {courseInstructor : instructor}=course; 
+
+console.log('final output : ',instructor);
+
+
+
+//we can access objects keys and values objectName.keys and objectName.values : it returns array of objects of keys and values
+
+
+
+
+
+
+
+
+
+
+
+
